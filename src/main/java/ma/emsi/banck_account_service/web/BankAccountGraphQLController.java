24961b7531e4b23d93ec52ab4c65cba177c877ba
Ghannane Intissar <ghannaneintissar@gmail.com>
2025-10-12 15:24:37 +0100

    commit 5

---
+++ src/main/java/ma/emsi/banck_account_service/web/BankAccountGraphQLController.java
@@ -0,0 +1,46 @@
+package ma.emsi.banck_account_service.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.emsi.banck_account_service.dto.BankAccountRequestDTO;
+import ma.emsi.banck_account_service.dto.BankAccountResponseDTO;
+import ma.emsi.banck_account_service.entities.BankAccount;
+import ma.emsi.banck_account_service.repositories.BankAccountRepository;
+import ma.emsi.banck_account_service.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @Autowired
+    private AccountService accountService;
+
+    @QueryMapping // when the client demands an accountsList that is in graphql, it will execute this method that is with same name
+    public List<BankAccount> accountsList()
+    {
+        return bankAccountRepository.findAll();
+    }
+    @QueryMapping // when the client demands an accountsList that is in graphql, it will execute this method that is with same name
+    public BankAccount bankAccountById(@Argument String id)
+    {
+        return bankAccountRepository.findById(id).orElseThrow(()->new RuntimeException(String.format("Bank Account %s not found",id)));
+    }
+
+    @MutationMapping // used for modification operations
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount){
+        return accountService.addAccont(bankAccount);
+    }
+
+
+
+    //an immutable object that lets you choose what to create using a constructor
+    //record BankAccountDTO(Double balance,String type,String currency){}
+}
